b2a26a3084825a8c52de55eec99dbf6a47616b9d
rochagi <grocha0507@gmail.com>
2023-11-20 02:42:47 -0300

    first commit for the Pictures swift application

---
+++ project1/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  project1
+//
+//  Created by Giovanna Rocha on 17/11/23.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items{
+            if item.hasPrefix("nssl"){
+                pictures.append(item)
+            }
+        }
+        print(pictures)
+        // Do anpy additional setup after loading the view.
+    }
+
+
+}
+
