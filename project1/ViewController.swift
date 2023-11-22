84c451e0f85ed861bf16fcce946d2e4b9e5577f0
rochagi <grocha0507@gmail.com>
2023-11-21 22:51:59 -0300

    add image view

--- project1/ViewController.swift
+++ project1/ViewController.swift
@@ -25,7 +25,15 @@ class ViewController: UITableViewController {
         print(pictures)
         // Do anpy additional setup after loading the view.
     }
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
 
 }
 
